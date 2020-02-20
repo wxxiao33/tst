36060259b751f2884720e3aaf9f9d83ef5d289ce
Kuan-Yu Chen <chenky@brandeis.edu>
2020-02-19 20:22:30 -0500

    Add test/integration/homepage_layout_test.rb

---
+++ test/integration/homepage_layout_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class HomepageLayoutTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "homepage links" do
+  	get root_path
+  	assert_template 'home_page/home'
+	assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", challenges_path
+    assert_select "a[href=?]", '/users/new'
+    assert_select "a[href=?]", '/challenges/new'
+  end
+end
