407d37d2fce3a9e918e66272aecbe3ebd4639ad5
Anyan <anyanxie@gmail.com>
2020-03-18 22:29:41 -0400

    added template for challenge picture

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ require("channels");
 
 import "bootstrap";
 import "../stylesheets/application";
+import "./custom";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
