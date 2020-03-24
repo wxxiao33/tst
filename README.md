da729bda8e74873b345748115a96727154b57b1a
Kuan-Yu Chen <chenky@brandeis.edu>
2020-03-23 20:07:41 -0400

    Update update_README.sh

--- README.md
+++ README.md
@@ -3,7 +3,6 @@ Please add `# FIXME` wherever you see something that needs fixing.
 Before pushing to master, run `./update_README.sh`.
 
 ==========FIXME==========
-
 app/controllers/pages_controller.rb:
   * [44] flash persists (kaunyu)
 
--- update_README.sh
+++ update_README.sh
@@ -1,5 +1,5 @@
-echo "heroku: https://one-day-brandeis.herokuapp.com/" > README.md
-echo "Please add \`# FIXME\` wherever you see something that needs fixing." >> README.md 
+printf "heroku: https://one-day-brandeis.herokuapp.com/\n" > README.md
+printf "Please add \`# FIXME\` wherever you see something that needs fixing.\n" >> README.md 
 printf "Before pushing to master, run \`./update_README.sh\`.\n\n" >> README.md 
-echo '==========FIXME==========' >> README.md 
+printf '==========FIXME==========' >> README.md 
 rails notes:fixme >> README.md
