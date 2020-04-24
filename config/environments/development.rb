106f18c233483cfa949ae2266d291bd0df75946f
wxxiao33 <infinityxwx123@gmail.com>
2020-04-23 21:07:55 -0400

    use_a_new_database

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,8 +37,8 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     domain: "gmail.com",
     port: 587,
-    # user_name: "onedaychallege@gmail.com",
-    # password: "OneDay!!",
+    user_name: "onedaychallege@gmail.com",
+    password: "OneDay!!",
     authentication: 'plain',
     enable_starttls_auto: true
   }
